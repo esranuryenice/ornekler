e951d4fc2456371b00029534c8c7a6965509112d
esranur.yenice <esranur.yenice@ALS-YG39104-J04>
2016-11-29 10:18:25 +0200

    swicth case ile örnekler

---
+++ switch_case/switch_case/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace switch_case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int gun = Convert.ToInt32(Console.ReadLine());
+            //switch (gun)
+            //{
+            //    case 1:
+            //        Console.WriteLine("pazartesi");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("salı");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("carsamba");
+            //        break;
+            //    case 4:
+            //        Console.WriteLine("perşembe");
+            //        break;
+            //    case 5:
+            //        Console.WriteLine("cuma");
+            //        break;
+            //    case 6:
+            //        Console.WriteLine("cumartesi");
+            //        break;
+            //    case 7:
+            //        Console.WriteLine("pazar");
+            //        break;
+            //    default:
+            //        Console.WriteLine("yanlış Giriş!");
+            //        break;
+            //}
+            //Console.ReadKey();
+
+            baş:
+            int gun = Convert.ToInt32(Console.ReadLine());
+            switch (gun)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("Kış");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("İlkbahar");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("Yaz");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("Sonbahar");
+                    break;
+
+            }
+            if (gun > 0)
+            {
+                goto baş;
+            }
+
+        }
+    }
+}
---
+++ switch_case2/switch_case2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace switch_case2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   bas:
+            Console.Write("1. sayiyi girin=" + " ");
+            double sayi1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("2. sayiyi girin=" + " ");
+            double sayi2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("İşlemi Girin="+ " ");
+            char islem = Convert.ToChar(Console.ReadLine());
+            switch (islem)
+            {
+                case ('+'):
+                    Console.Write("Sayilarin Toplami= "+" " + (sayi1 + sayi2));
+                    break;
+                case ('-'):
+                    Console.Write("Sayilarin Cikartmasi=" + "  "  + (sayi1 - sayi2));
+                    break;
+                case ('*'):
+                    Console.Write("Sayilarin Carpımı=" + " " + (sayi1 * sayi2));
+                    break;
+                case ('/'):
+                    Console.Write("Sayilarin Bolümü=" + " " + (sayi1 / sayi2));
+                    break;
+                default:
+                    Console.WriteLine("\nYanlis Bir Secim Yaptınız \nLütfen Yeniden Secim Yapın\n");
+                    goto bas;
+
+            }
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
