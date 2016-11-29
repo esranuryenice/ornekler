d099d0ee1e85207ed234e5f13973e576795e2ec4
esranur.yenice <esranur.yenice@ALS-YG39104-J04>
2016-11-29 11:44:26 +0200

    sdf

---
+++ While_Döngüsü/While_Döngüsü/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While_Döngüsü
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region [0,9] sayilari yaz
+
+
+            //int sayac = 0;
+            //while (sayac < 10)
+            //{
+            //    Console.WriteLine(sayac);
+            //    sayac++;
+            //}
+            //    Console.ReadKey();
+            #endregion
+
+
+            #region [0,n] sayilari toplayan
+
+            //int n = 1000;
+            //int sayac = 0;
+            //int toplam = 0;
+            //while (sayac <= n)
+            //{
+            //    toplam = toplam + sayac;
+            //     sayac++;
+           
+            //}
+            //Console.WriteLine("sonuc=" + toplam);
+
+            //Console.ReadKey();
+            #endregion
+
+
+           
+
+
+
+        }
+    }
+}
