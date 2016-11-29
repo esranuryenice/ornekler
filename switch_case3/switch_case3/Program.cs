87ce9214869beb270f54478aa60425551d026ed6
esranur.yenice <esranur.yenice@ALS-YG39104-J04>
2016-11-29 11:17:13 +0200

    musteri hizmetleri örnegi

--- switch_case3/switch_case3/Program.cs
+++ switch_case3/switch_case3/Program.cs
@@ -10,7 +10,34 @@ namespace switch_case3
     {
         static void Main(string[] args)
         {
-            //5oıj
+            #region MusteriHizmetleri
+            Console.WriteLine("Merhaba Lütfen yapmak istediginiz islemi seciniz..");
+            Console.WriteLine("1.Lira Yükleme");
+            Console.WriteLine("2.İnternet Paketi");
+            Console.WriteLine("3.Konuşma Paketi");
+            int secim = Convert.ToInt32(Console.ReadLine());
+             switch(secim)
+            {
+                case 1:
+                    Console.WriteLine("Lira Yükleme servisine Hoş Geldiniz");
+                    break;
+                case 2:
+                    Console.WriteLine("İnternet Paketi servisine Hoş Geldiniz");
+                    break;
+                case 3:
+                    Console.WriteLine("Konuşma Paketi servisine Hoş Geldiniz");
+                    break;
+                default:
+                    Console.WriteLine("Müşteri Temsilcisine Yönlendiriliyorsunuz...");
+                    break;
+              }
+            Console.ReadKey();
+                    
+ #endregion
+
+
+            
+           
         }
     }
 }
