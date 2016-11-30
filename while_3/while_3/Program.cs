9b757661df9a3a7d2a04d660610052500f85a3b1
esranur.yenice <esranur.yenice@ALS-YG39104-J08>
2016-11-30 10:09:38 +0200

    sayiyitoplayanişlemseçtiren

---
+++ while_3/while_3/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace while_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region sayiyitoplayanişlemseçtiren
+
+
+            bool devam = true;
+            while (devam)
+            {
+                Console.Write("Birinci Sayı : ");
+                double sayi1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("İkinci Sayı : ");
+                double sayi2 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("İşlem Seç : ");
+                char islem = Convert.ToChar(Console.ReadLine());
+
+                double sonuc = 0;
+                switch (islem)
+                {
+                    case '+':
+                        sonuc = sayi1 + sayi2;
+                        break;
+                    case '-':
+                        sonuc = sayi1 - sayi2;
+                        break;
+                    case '*':
+                        sonuc = sayi1 * sayi2;
+                        break;
+                    case '/':
+                        sonuc = sayi1 / sayi2;
+                        break;
+                    default:
+                        Console.WriteLine("Böyle Bir işlem tipi yok");
+                        break;
+                }
+                Console.WriteLine("Sonuc = " + sonuc);
+                Console.WriteLine("Devam Edelim Mi?(Evet) ");
+                string cevap = Console.ReadLine();
+                if (cevap == "evet")
+                {
+
+                    devam = true;
+
+                }
+                else
+                {
+                    devam = false;
+                }
+            }
+            Console.WriteLine("Görüşmek Üzere");
+            Console.ReadKey();
+            #endregion
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
