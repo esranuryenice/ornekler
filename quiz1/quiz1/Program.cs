f8bb019854fe0830faf6b0f57673a79e89aeb0c4
esranur.yenice <esranur.yenice@ALS-YG39104-J01>
2016-12-01 10:12:06 +0200

    s

---
+++ quiz1/quiz1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quiz1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int toplam = 0;
+            for (int i = 1; i < 10; i++)
+            {
+                toplam = toplam + i;
+
+               // for (int j = 1; j <=10 ; j--)
+               // {
+               //     Console.WriteLine(j);
+               // }
+                Console.WriteLine("Toplam = " + i);
+                
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
