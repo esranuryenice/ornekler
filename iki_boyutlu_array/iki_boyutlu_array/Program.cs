c83ecb3bc66eee90d3f499b7b6f0c2c24892e5af
esranur.yenice <esranur.yenice@ALS-YG39104-J01>
2016-12-06 09:49:33 +0200

    k

--- iki_boyutlu_array/iki_boyutlu_array/Program.cs
+++ iki_boyutlu_array/iki_boyutlu_array/Program.cs
@@ -10,6 +10,19 @@ namespace iki_boyutlu_array
     {
         static void Main(string[] args)
         {
+            int[,] sinavlar = new int[2, 3];
+            sinavlar[0, 0] = 1;
+            sinavlar[1, 0] = 20;
+            sinavlar[0, 1] = 2;
+            sinavlar[1, 1] = 40;
+            sinavlar[0, 2] = 3;
+            sinavlar[1, 2]= 60;
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(sinavlar[0,i]+ " - " + sinavlar[1,i]);
+            }
+            Console.ReadKey();
+
         }
     }
 }
