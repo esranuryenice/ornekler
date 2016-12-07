5a95680d9162993ab8cad1a30ccd2052a4c64fe2
esranur.yenice <esranur.yenice@ALS-YG39104-J01>
2016-12-07 11:59:33 +0200

    form

---
+++ HelloWorld_WinForms/HelloWorld_WinForms/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloWorld_WinForms
+{
+    public partial class IlkForm : Form
+    {
+        public IlkForm()
+        {
+            InitializeComponent();
+        }
+
+        private void IlkForm_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Merhaba İzmir");
+        }
+
+       
+    }
+}
---
+++ Ilk_deneme_form/Ilk_deneme_form/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ilk_deneme_form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           // MessageBox.Show("Merhaba");
+        }
+
+        private void KaydetBtn_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ad" + ad_txt.Text + "\nsifre" + sifre_txt.Text);
+        }
+    }
+}
